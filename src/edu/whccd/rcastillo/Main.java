7da9324ec754ed599fbd1615d270a0e071179a01
RichardCastillo <armed_deity@hotmail.com>
2020-05-02 09:33:17 -0700

    Initial commit

---
+++ src/edu/whccd/rcastillo/Main.java
@@ -0,0 +1,27 @@
+package edu.whccd.rcastillo;
+
+import java.util.Scanner;;import static java.util.Scanner.*;
+
+/* This custom application is to show the use of exeption handling
+    Author: Richard Castillo
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+
+	System.out.println("Enter a Integer: ");
+	int userInput = Scanner.nextInt();
+	try {
+	     System.out.println(userInput + "is valid");
+    } catch (Exception e) {
+	    System.out.println("Invalid Input");
+    } finally {
+	    System.out.println("This finally block will run regardless if there is an exception or not");
+    }
+
+
+    }
+}
