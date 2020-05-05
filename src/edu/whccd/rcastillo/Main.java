9e4a9a97a4fafcaec97f9ade5b3cf4e646e6475d
RichardCastillo <armed_deity@hotmail.com>
2020-05-05 06:41:51 -0700

    fixed sc.scanner, and system.out. placed within try block.

--- src/edu/whccd/rcastillo/Main.java
+++ src/edu/whccd/rcastillo/Main.java
@@ -1,6 +1,6 @@
 package edu.whccd.rcastillo;
 
-import java.util.Scanner;;import static java.util.Scanner.*;
+import java.util.Scanner;
 
 /* This custom application is to show the use of exception handling
     Author: Richard Castillo
@@ -9,15 +9,19 @@ import java.util.Scanner;;import static java.util.Scanner.*;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
+		int userInput;
+    	Scanner sc = new Scanner(System.in);
 
 
-	System.out.println("Enter a Integer: ");
-	int userInput = Scanner.nextInt();
+
+
 	try {
-	     System.out.println(userInput + "is valid");
+		System.out.print("Enter a Integer: ");
+		userInput = sc.nextInt();
+		System.out.println(userInput + " is valid");
+
     } catch (Exception e) {
-	    System.out.println("Invalid Input");
+	    System.out.println(" Invalid Input");
     } finally {
 	    System.out.println("This finally block will run regardless if there is an exception or not");
     }
